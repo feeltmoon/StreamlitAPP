515e424bf2cc1670307cc121b956c1bd3230183a
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-04-29 22:54:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,16 +17,29 @@ def generate_reports(uploaded_files):
     if uploaded_files is not None:
         zip_file = zipfile.ZipFile("data_download.zip", mode="w")
         
-        
+        #read df1
         file_name_sugg = 'Medidata Rave EDC Roles Assignment and Quarterly Review Suggestions.xlsx'                                       
-        file_sugg_obj = find_file(file_name_sugg,uploaded_files)
-        
+        file_sugg_obj = find_file(file_name_sugg,uploaded_files)      
+        #read df1
         if file_sugg_obj is not None:
             bytes_data = file_sugg_obj.read()
             df1 = pd.read_excel(io.BytesIO(bytes_data),sheet_name='Live Contact List - Other',header=1, engine='openpyxl')
-            st.write(df1)
-            zip_file.writestr(file_sugg_obj.name, bytes_data)
-            
+            df1['Role'] = df1['Role'].astype(str)
+            df1['Role'] = df1['Role'].apply(lambda x: x.split('/')).explode().reset_index(drop=True)                
+            df1['Role'] = df1['Role'].str.lstrip()
+            df1['Role'] = df1['Role'].str.rstrip()
+            st.write(df1)          
+            output_df1_name = f"{file_sugg_obj.name.split('.')[0]}_df1.xlsx"          
+            zip_file.writestr(output_df1_name, bytes_data)
+            #read df2
+            df2 = pd.read_excel(io.BytesIO(bytes_data),sheet_name='Country Codes',usecols=['Country/Region Name','6 Digit Code'],engine='openpyxl')
+            df2 = df2.loc[~df2['Country/Region Name'].isna(),:]
+            df2['Code_Sub'] = df2['6 Digit Code'].str[:3]
+            df2 = df2.drop_duplicates(subset='Code_Sub',keep='last')
+            df2 = df2.drop(columns='6 Digit Code')
+            st.write(df2)
+            output_df2_name = f"{file_sugg_obj.name.split('.')[0]}_df2.xlsx"
+            zip_file.writestr(output_df2_name, bytes_data)
 
         zip_file.close()
         
