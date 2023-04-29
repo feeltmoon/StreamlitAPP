e4f2426babff1b81dbd8f44a4799d488942d3fed
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-04-29 15:32:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,54 +3,37 @@ import pandas as pd
 import streamlit as st
 import os
 import openpyxl
-
+import zipfile
 
 def find_file(filename, path):
     for root, dirs, files in os.walk(path):
         if filename in files:
             return os.path.join(root, filename)
 
-def generate_reports(uploaded_files):
-        
-    # path = os.path.join(os.getcwd(), folder_name)
-    # sub_dir_srcfiles = os.path.join(path, 'Source')    
-    
-    # path_abs = os.path.abspath(path)
-    # sub_dir_srcfiles_abs = os.path.abspath(sub_dir_srcfiles)
-    
-    # if not os.path.isdir(sub_dir_srcfiles):
-    #     return 'Source Files folder does not exist.' + " : " + path_abs + " : " + sub_dir_srcfiles_abs
-    # file_name_sugg = 'Medidata Rave EDC Roles Assignment and Quarterly Review Suggestions.xlsx'
-    # file_path_sugg = find_file(file_name_sugg, sub_dir_srcfiles)
-    # if not file_path_sugg:
-    #     return 'Suggestion file is not uploaded.'
+def generate_reports(uploaded_files):        
 
-    if uploaded_files is not None:        
+    if uploaded_files is not None:     
+        output_files = []
+        
         for file in uploaded_files:    
             filename = file.name
-        #Read source files, df, df1, df2, df3
-        ##read df1 from suggestion
+            #Read source files, df, df1, df2, df3
+            ##read df1 from suggestion
             if "Suggestions" in filename:
-                df1 = pd.read_excel(file,sheet_name='Live Contact List - Other',header=1)
-                #df1 = df1.apply(lambda x: x.str.split('/').explode()).reset_index()
-                
+                df1 = pd.read_excel(file,sheet_name='Live Contact List - Other',header=1)           
                 df1['Role'] = df1['Role'].astype(str)
-                df1['Role'] = df1['Role'].apply(lambda x: x.split('/')).explode().reset_index(drop=True)  
-                
+                df1['Role'] = df1['Role'].apply(lambda x: x.split('/')).explode().reset_index(drop=True)                
                 df1['Role'] = df1['Role'].str.lstrip()
                 df1['Role'] = df1['Role'].str.rstrip()
                 ## debug_output:
-                st.write(df1)
+                #st.write(df1)
                 
-                # Add a download button to allow the user to download the DataFrame as an Excel file
-                output_file_name = "df1"
-                #output_file_name2 = "df2.xlsx"
-                #output_file_name3 = "df3.xlsx"
+                # Generate the output file name based on the input file name
+                output_file_name = f"{filename.split('.')[0]}_output.xlsx"
+                # Convert the DataFrame to an Excel file and add it to the list of output files
                 output_file_contents = df1.to_excel(index=False, header=True)
-                
-                st.download_button(label="Download output file", data=output_file_contents, file_name=output_file_name, mime="application/vnd.ms-excel")
-                
-                
+                output_files.append((output_file_name, output_file_contents))
+              
                 #df1.to_excel(path + '\\df1_debug.xlsx')
                 ## read df2 from suggestion
                 df2 = pd.read_excel(file,sheet_name='Country Codes',usecols=['Country/Region Name','6 Digit Code'])
@@ -58,31 +41,28 @@ def generate_reports(uploaded_files):
                 df2['Code_Sub'] = df2['6 Digit Code'].str[:3]
                 df2 = df2.drop_duplicates(subset='Code_Sub',keep='last')
                 df2 = df2.drop(columns='6 Digit Code')
-                st.write(df2)
-                #df2.to_excel(path + '\\df2.xlsx',index=False)
-    #print(len(df2))
-    ## debug_output:
-    #df2.to_excel(path + '\\df2_debug.xlsx',index=False)  
-    ##read df3
-    # file_name_nmlst = 'Name List.xlsx'
-    # file_path_nmlst = find_file(file_name_nmlst,sub_dir_srcfiles)                
-    # if file_path_nmlst == "":
-    #     return 'name list file is not uploaded.'
-    #     os._exit(0)
-            if "name list" in filename:               
-                df3 = pd.read_excel(file,sheet_name='名录（按组织）')
-                df3 = df3.rename(columns={'电子邮件地址': 'Email_Source', '职务头衔':'Title'})
-                def GetEmailAddress(x):
-                    return x.split('（')[0].strip(' ')
-                df3.loc[:,'Email'] = df3['Email_Source'].astype(str).apply(lambda x: GetEmailAddress(x))
-                df3 = pd.DataFrame(df3,columns=(['Email','Title']))
-                df3 = df3.drop_duplicates(subset='Email')
-                df3.loc[:,'Email_Upper'] = df3.loc[:,'Email']
-                df3.loc[:,'Email_Upper'] = df3.apply(lambda x: x.str.upper())
-                df3 = df3.drop(columns='Email')
-                ##debug_output:
-                #df3.to_excel(path + '\\df3_debug.xlsx')
-                st.write(df3)
+                    
+                # Generate the output file name based on the input file name
+                output_file_name_df2 = f"{filename.split('.')[0]}_output.xlsx"
+                # Convert the DataFrame to an Excel file and add it to the list of output files
+                output_file_contents_df2 = df2.to_excel(index=False, header=True)
+                output_files.append((output_file_name_df2, output_file_contents_df2))
+                    
+                    
+            # if "name list" in filename:               
+            #     df3 = pd.read_excel(file,sheet_name='名录（按组织）')
+            #     df3 = df3.rename(columns={'电子邮件地址': 'Email_Source', '职务头衔':'Title'})
+            #     def GetEmailAddress(x):
+            #         return x.split('（')[0].strip(' ')
+            #     df3.loc[:,'Email'] = df3['Email_Source'].astype(str).apply(lambda x: GetEmailAddress(x))
+            #     df3 = pd.DataFrame(df3,columns=(['Email','Title']))
+            #     df3 = df3.drop_duplicates(subset='Email')
+            #     df3.loc[:,'Email_Upper'] = df3.loc[:,'Email']
+            #     df3.loc[:,'Email_Upper'] = df3.apply(lambda x: x.str.upper())
+            #     df3 = df3.drop(columns='Email')
+            #     ##debug_output:
+            #     #df3.to_excel(path + '\\df3_debug.xlsx')
+            #     st.write(df3)
             
             return "no file meets the requirements."
                 
@@ -93,6 +73,18 @@ def generate_reports(uploaded_files):
             # output_file_contents = df1.to_excel(index=False, header=True)
             
             # st.download_button(label="Download output file", data=output_file_contents, file_name=output_file_name, mime="application/vnd.ms-excel")
+
+    # Create a zip file containing all the output files
+    zip_file_name = "output.zip"
+    with zipfile.ZipFile(zip_file_name, "w") as zip_file:
+        for output_file in output_files:
+            zip_file.writestr(output_file[0], output_file[1])
+    # Add a download button to allow the user to download the zip file
+    with open(zip_file_name, "rb") as f:
+        zip_file_contents = f.read()
+    st.download_button(label="Download all output files as a zip", data=zip_file_contents, file_name=zip_file_name, mime="application/zip")
+            
+            
 
     # df1.to_excel(path + '\\df1_debug.xlsx')
     # df2.to_excel(path + '\\df2_debug.xlsx')
