5a4455b23e079558eec716e45fe7539421e9561a
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-04-29 21:02:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,25 @@ import os
 import openpyxl
 import zipfile
 import io
+import base64
 
 def find_file(filename, uploaded_files):
     for file in uploaded_files:
         if filename in file.name:
             return file
 
+def generate_zip_download_link(zip_file):
+    towrite = io.BytesIO()
+    zip_file.save(towrite)
+    towrite.seek(0)
+    b64 = base64.b64encode(towrite.read()).decode()
+    href = f'<a href="data:application/zip;base64,{b64}" download="data_download.zip">Download Zip File</a>'
+    return st.markdown(href, unsafe_allow_html=True)
+
 def generate_reports(uploaded_files):
 
     if uploaded_files is not None:     
-        output_files = []
+        dfs = []
         
         for file in uploaded_files:    
             filename = file.name
@@ -25,8 +34,7 @@ def generate_reports(uploaded_files):
             file_name_sugg = 'Medidata Rave EDC Roles Assignment and Quarterly Review Suggestions.xlsx'                                       
             file_path_sugg = find_file(file_name_sugg,uploaded_files)
             
-            st.write(file_path_sugg.name)
-            df1 = pd.read_excel(file_path_sugg,sheet_name='Live Contact List - Other',header=1)           
+            df1 = pd.read_excel(file_path_sugg,sheet_name='Live Contact List - Other',header=1,engine='openpyxl')           
             # df1['Role'] = df1['Role'].astype(str)
             # df1['Role'] = df1['Role'].apply(lambda x: x.split('/')).explode().reset_index(drop=True)                
             # df1['Role'] = df1['Role'].str.lstrip()
@@ -34,17 +42,37 @@ def generate_reports(uploaded_files):
             # ## debug_output:
             # #st.write(df1)          
             # Generate the output file name based on the input file name
-            output_file_name = f"{file_path_sugg.name.split('.')[0]}_output.xlsx"
-            st.write(output_file_name)
-            # Convert the DataFrame to an Excel file and add it to the list of output files
-            #output_file_contents = df1.to_excel('df1_debug.xlsx',engine='openpyxl')
-                        
-            output_file_contents = io.BytesIO()
-            with pd.ExcelWriter(output_file_contents, engine='openpyxl') as writer:
-                df1.to_excel(writer, index=False)
-            output_file_contents.seek(0)
             
-            output_files.append((output_file_name, output_file_contents))
+
+            dfs.append(df1)
+            st.write(f'File name: {file.name}')
+            
+            
+        if len(dfs) > 0:
+            zip_file = zipfile.ZipFile('data_download.zip', 'w')
+            for i in range(len(dfs)):
+                df = dfs[i]
+                filename = f'data_{i}.xlsx'
+                df.to_excel(filename, encoding="utf-8", index=False)
+                zip_file.write(filename)
+            zip_file.close()
+            generate_zip_download_link(zip_file)
+            
+            
+           
+            ##backup
+            # output_file_name = f"{file_path_sugg.name.split('.')[0]}_output.xlsx"
+            # st.write(output_file_name)
+            # # Convert the DataFrame to an Excel file and add it to the list of output files
+            # #output_file_contents = df1.to_excel('df1_debug.xlsx',engine='openpyxl')
+            
+            # output_file_contents = io.BytesIO()
+            # with pd.ExcelWriter(output_file_contents, engine='openpyxl') as writer:
+            #     df1.to_excel(writer, index=False)
+            # output_file_contents.seek(0)
+            
+            # output_files.append((output_file_name, output_file_contents))
+            # ##backup
             
             # st.write(type(output_files))
             # st.write(len(output_files))
@@ -77,16 +105,17 @@ def generate_reports(uploaded_files):
             #     ##debug_output:
             #     #df3.to_excel(path + '\\df3_debug.xlsx')
             #     st.write(df3)            
-                
-        # Create a zip file containing all the output files
-        zip_file_name = "output.zip"
-        with zipfile.ZipFile(zip_file_name, "w") as zip_file:
-            for output_file in output_files:
-                zip_file.writestr(output_file[0], output_file[1])
-        # Add a download button to allow the user to download the zip file
-        with open(zip_file_name, "rb") as f:
-            zip_file_contents = f.read()
-        st.download_button(label="Download all output files as a zip", data=zip_file_contents, file_name=zip_file_name, mime="application/zip")
+        
+        # #backup
+        # # Create a zip file containing all the output files
+        # zip_file_name = "output.zip"
+        # with zipfile.ZipFile(zip_file_name, "w") as zip_file:
+        #     for output_file in output_files:
+        #         zip_file.writestr(output_file[0], output_file[1])
+        # # Add a download button to allow the user to download the zip file
+        # with open(zip_file_name, "rb") as f:
+        #     zip_file_contents = f.read()
+        # st.download_button(label="Download all output files as a zip", data=zip_file_contents, file_name=zip_file_name, mime="application/zip")
                 
                 
 
