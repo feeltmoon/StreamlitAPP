0b1a1406a4084fa203752baad2bcf15b0d5d922b
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-04-29 15:52:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def generate_reports(uploaded_files):
             filename = file.name
             #Read source files, df, df1, df2, df3
             ##read df1 from suggestion
-            if "Suggestions" in filename:
+            if "Medidata Rave EDC Roles Assignment and Quarterly Review Suggestions" in filename:
                 df1 = pd.read_excel(file,sheet_name='Live Contact List - Other',header=1)           
                 df1['Role'] = df1['Role'].astype(str)
                 df1['Role'] = df1['Role'].apply(lambda x: x.split('/')).explode().reset_index(drop=True)                
