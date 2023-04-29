af480fc14cdd20c40eb5f2804f8813e7d5af36e8
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-04-29 22:40:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,61 @@ import zipfile
 import base64
 import openpyxl
 
-def main():
-    st.title("Multiple Excel Files Uploader")
-    uploaded_files = st.file_uploader("Choose your Excel files", accept_multiple_files=True, type=['xlsx'])
-    if uploaded_files:
+
+def find_file(filename, uploaded_files):
+    for file in uploaded_files:
+        if filename in file.name:
+            return file
+
+
+def generate_reports(uploaded_files):
+    
+    if uploaded_files is not None:
         zip_file = zipfile.ZipFile("data_download.zip", mode="w")
-        for uploaded_file in uploaded_files:
-            bytes_data = uploaded_file.read()
-            df = pd.read_excel(io.BytesIO(bytes_data),engine='openpyxl')
-            st.write(df)
-            zip_file.writestr(uploaded_file.name, bytes_data)
+        
+        
+        file_name_sugg = 'Medidata Rave EDC Roles Assignment and Quarterly Review Suggestions.xlsx'                                       
+        file_sugg_obj = find_file(file_name_sugg,uploaded_files)
+        
+        if file_sugg_obj is not None:
+            bytes_data = file_sugg_obj.read()
+            df1 = pd.read_excel(io.BytesIO(bytes_data),sheet_name='Live Contact List - Other',header=1, engine='openpyxl')
+            st.write(df1)
+            zip_file.writestr(file_sugg_obj.name, bytes_data)
+            
+
         zip_file.close()
+        
+        
+        
+        #Generate download zip button
         with open("data_download.zip", "rb") as f:
             bytes = f.read()
             b64 = base64.b64encode(bytes).decode()
         href = f'<a href="data:application/zip;base64,{b64}" download="data_download.zip">Download Zip File</a>'
         st.markdown(href, unsafe_allow_html=True)
 
+
+
+
+
+
+def main():
+ 
+    #Title
+    st.title("Multiple Excel Files Uploader")
+    #Dropdown list
+    options = ["QuarterlyReview", "Option 2", "Option 3"]
+    selected_option = st.selectbox("Choose an option", options)
+    #Uploader
+    uploaded_files = st.file_uploader("Choose your files", accept_multiple_files=True, type=['xlsx'])
+    #Execute Buttion
+    button = st.button('Create Folder')
+
+    if button:
+        status = generate_reports(uploaded_files)
+        st.write(status)
+        
+    
 if __name__ == "__main__":
     main()
